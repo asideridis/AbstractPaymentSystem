41c7dd784993672a8867cf664bac82d7494c7cc2
Thanasis Sideridis <tsideridis@gmail.com>
2024-10-30 08:12:42 +0200

    Abstract Mapping Payment type to procedure with naming convention

--- PaymentService/Strategies/CreditCardPayment.cs
+++ PaymentService/Strategies/CreditCardPayment.cs
@@ -1,5 +1,6 @@
 ﻿namespace PaymentService.Strategies;
 
+//Naming convention: <PaymentMethod>Payment
 public class CreditCardPayment :PaymentMethodBase
 {
     protected override void ProcessPaymentCore(decimal amount)
